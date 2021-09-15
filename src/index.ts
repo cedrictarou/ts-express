52e39c258b6138356fdedbc1bd3cc475564ba9d5
cedrictaro-g <cedric.tarou@gmail.com>
2021-09-16 06:54:59 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response, NextFunction } from 'express'
+import todoRoutes from './routes/todos'
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.json('Hello World')
+})
+app.use('/todos', todoRoutes)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message })
+})
+
+app.listen(3000)
